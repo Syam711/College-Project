88a9c2a7a22a82c8457d4a49d42b82a87bf12794
Syam711 <bashyamshyam123@gmail.com>
2024-12-22 23:48:34 +0530

    Created a home page to upload the image

--- svisions/views.py
+++ svisions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
 
-# Create your views here.
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'svisions/upload_success.html')
+    else:
+        form = ImageUploadForm()
+    return render(request, 'svisions/upload_image.html', {'form': form})
