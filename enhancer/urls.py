c6597797ad057e93cb41c528cee2bd6e267da369
Basyam Syam Kumar <156494140+Syam711@users.noreply.github.com>
2025-04-16 23:04:53 +0530

    Update urls.py

--- enhancer/urls.py
+++ enhancer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_page, name='home'),
     path('upload/', views.upload_image, name='upload_image'),
     path("success/", views.success_page, name="success"),
 ]
